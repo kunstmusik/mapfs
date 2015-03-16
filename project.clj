add656f9387bf9e55f3668ed1c5d1c156518c1b0
Steven Yi <stevenyi@gmail.com>
2015-03-15 21:23:52 -0400

    updated info

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject mapfs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject kunstmusik/mapfs "0.1.0-SNAPSHOT"
+  :description "Use Clojure map like a filesystem"
+  :url "http://github.com/kunstmusik/mapfs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
